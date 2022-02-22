1aa520064e65b9ba1a002f840692617561719f62
Valdesrep <90558354+Valdesrep@users.noreply.github.com>
2022-02-22 17:52:02 +0200

    9дз таск 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBnt: document.querySelector('[data-stop]'),
+  body: document.body,
+};
+let timerId = null;
 
+refs.startBtn.addEventListener('click', onStartColorSwitch);
+refs.stopBnt.addEventListener('click', onStopColorSwinch);
+
+function onStartColorSwitch() {
+  refs.startBtn.disabled = true;
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function onStopColorSwinch() {
+  refs.startBtn.disabled = false;
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
