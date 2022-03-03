70c6fa6fd338b875ec57cb124bfeff39190fcaee
Valdesrep <90558354+Valdesrep@users.noreply.github.com>
2022-03-03 15:19:00 +0200

    конец 9 дз

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  const firstDelay =  Number(form.elements.delay.value);  
+  const stepDelay = Number(form.elements.step.value);  
+  const amount = Number(form.elements.amount.value);
+  let delay = firstDelay;
+
+  if (amount <= 0) {
+    Notify.warning('Enter a positive number');
+    return;
+  }
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += stepDelay;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
